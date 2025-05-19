ce3a078a0afb9bc6a0a146005923c90a35c67be5
afaslah <afaslah12@gmail.com>
2025-05-19 13:41:31 +0700

    web html

--- dsproject/dsapp/urls.py
+++ dsproject/dsapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
-urlPatterns = [
-    
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
